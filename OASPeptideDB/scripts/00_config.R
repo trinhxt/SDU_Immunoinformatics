b638979ba7fbda9d8b101911f1e2f171c43ab44c
trinhxt <xttrinh1199@gmail.com>
2026-02-01 23:26:57 +0100

    Update 00_config.R

--- OASPeptideDB/scripts/00_config.R
+++ OASPeptideDB/scripts/00_config.R
@@ -9,25 +9,25 @@
 # 1. Project Directories (Customize these)
 # ==============================================================================
 # Where the raw OAS shell script is located (Download this from OAS website)
-DOWNLOAD_SHELL_SCRIPT <- "data/bulk_download.sh"
+DOWNLOAD_SHELL_SCRIPT <- "data/bulk_download_human_unpaired_heavychain_disease.sh"
 
 # Where to store the raw downloaded files (can be deleted after processing)
-RAW_DATA_DIR <- "D:/OAS/raw"
+RAW_DATA_DIR <- "D:/OAS/unpaired/raw"
 
 # Where to save the cleaned, processed CSV.GZ files
-PROCESSED_DATA_DIR <- "D:/OAS/antibody"
+PROCESSED_DATA_DIR <- "D:/OAS/unpaired/antibody"
 
 # Where to save the tryptic peptide data (CSV.GZ file)
-TRYPTIC_DATA_DIR <- "D:/OAS/tryptic"
+TRYPTIC_DATA_DIR <- "D:/OAS/unpaired/tryptic"
 
 # Where to save the metadata CSV
-METADATA_FILE <- "D:/OAS/OAS_metadata.csv"
+METADATA_FILE <- "D:/OAS/unpaired/OAS_metadata.csv"
 
 # Where to build the final Parquet database
-PARQUET_DB_DIR <- "D:/OAS/tryptic/parquet_db"
+PARQUET_DB_DIR <- "D:/OAS/unpaired/tryptic/parquet_db"
 
-# Where to store logs and temporary files
-WORK_DIR <- "D:/OAS/temp"
+# Where to store temporary files
+WORK_DIR <- "E:/OAS_temp"
 
 # Reference file for filtering (Uniprot Tryptic Peptides)
 REF_PEPTIDE_RDATA <- "data/UniProtNCBI_Tryptic.RData"
@@ -40,7 +40,7 @@ REF_PEPTIDE_RDATA <- "data/UniProtNCBI_Tryptic.RData"
 N_CORES <- 6L
 
 # DuckDB settings for database compaction
-DUCKDB_MEMORY_LIMIT <- "40GB"
+DUCKDB_MEMORY_LIMIT <- "48GB"
 ROW_GROUP_SIZE <- 1000000L
 
 
@@ -68,13 +68,13 @@ P <- list(
   # Part 2 Specific Paths (Derived from WORK_DIR and PARQUET_DB_DIR)
   work_root       = norm_path(WORK_DIR),
   digest_csv_dir  = pjoin(TRYPTIC_DATA_DIR, "digest_csv_gz"),
-  log_dir         = pjoin(TRYPTIC_DATA_DIR, "Log_files"),
+  log_dir         = pjoin(TRYPTIC_DATA_DIR, "log_files"),
   staging_root    = pjoin(PARQUET_DB_DIR, "_staging"),
-  db_state1       = pjoin(PARQUET_DB_DIR, "db_state1"),
+  db_stage1       = pjoin(PARQUET_DB_DIR, "db_stage1"),
   
   ref_rdata       = norm_path(REF_PEPTIDE_RDATA)
 )
 
 message("Configuration loaded.")
 message(" - Processed Input: ", P$processed_dir)
-message(" - Final Output DB: ", P$db_state1)
+message(" - Final Output DB: ", P$db_stage1)
